8b4151bc3788d9a3a0d3ee8d1fddbec45f2961d1
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-01-05 17:43:16 +0530

    add

--- src/assets/Orders.jsx
+++ src/assets/Orders.jsx
@@ -6,31 +6,31 @@ import { IoCalendarOutline } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
 
 
-
-
 const Orders = () => {
   const { orders, orderSpin, filterOrders, setFilterOrders } = useContext(dataContext)
   const [todayOrders, setTodayOrders] = useState([])
   const [todayOrders1, setTodayOrders1] = useState([])
-  const [selectDate, setSelectDate] = useState(new Date().toLocaleDateString("en-GB"))
+  const [selectDate, setSelectDate] = useState(() => {
+    const today = new Date();
+    return today.toISOString().split("T")[0];
+  });
   const dateRef = useRef(null)
 
 
   // rendering orders according to the date 
   useEffect(() => {
-    const remainTodayOrders = orders.filter((item) => item.orderDate === selectDate)
+    const formattedDate = new Date(selectDate).toLocaleDateString("en-GB");
+    const remainTodayOrders = orders.filter((item) => item.orderDate === formattedDate)
     setTodayOrders(remainTodayOrders)
     setTodayOrders1(remainTodayOrders)
 
   }, [filterOrders, selectDate])
 
 
-  // converting date format 
+
   const dateHandleFunc = (event) => {
-    const eventDate = event.target.value
-    const [year, month, day] = eventDate.split("-")
-    const dateFormat = `${day}/${month}/${year}`
-    setSelectDate(dateFormat)
+    const eventDate = event.target.value;
+    setSelectDate(eventDate);
   }
 
   const handleLabelClick = () => {
@@ -74,7 +74,7 @@ const Orders = () => {
         <div className='flex justify-between w-full mt-5'>
           <div className='relative h-10 w-[7.2rem] cursor-pointer'>
             <label onClick={handleLabelClick} htmlFor="selectDate" className='border-2 w-full cursor-pointer h-full rounded border-blue-500 flex items-center justify-between px-1'>
-              {selectDate} <IoCalendarOutline />
+              {new Date(selectDate).toLocaleDateString("en-GB")} <IoCalendarOutline />
             </label>
             <input type="date" className=' w-full absolute inset-0 opacity-0  h-full cursor-pointer' ref={dateRef} value={selectDate} onChange={dateHandleFunc} name="selectDate" id="selectDate" />
           </div>
@@ -116,13 +116,13 @@ const Orders = () => {
               <div className='lg:flex lg:flex-wrap lg:justify-center lg:gap-4 '>
                 {todayOrders.map((item) => (
                   <Link to={`/order_over_view/${item._id}`} key={item._id} className='block lg:w-[35%] border bg-orange-300 hover:shadow-gray-400 relative  mb-3 shadow-md rounded shadow-gray-300 p-3'>
-                      <h6 className='text-white rounded p-1 px-2 bg-blue-500 w-fit'>Order From :  <span className='font-semibold capitalize text-white'>{item.shippingAddress[0].name}</span></h6>
-                      <h6 className='text-gray-600 mt-1'>Total Amount : <span className='font-semibold capitalize text-black'>₹{item.totalAmount}</span></h6>
-                      <h6 className='text-gray-600 mt-1'>Order Status : <span className={`font-semibold capitalize ${item.orderStatus === "pending" ? "bg-white text-orange-700 rounded p-1 px-2" : ""} ${item.orderStatus === "cancelled" ? "bg-red-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "confirmed" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "delivered" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "shipped" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "outofdelivery" ? "bg-green-600 text-white rounded p-1 px-2" : ""}`}>{item.orderStatus}</span></h6>
-                      <h6 className='text-gray-600 mt-1'>Address : <span className='font-semibold capitalize text-black'>{item.shippingAddress[0].city}</span></h6>
-                      <h6 className='text-gray-600 mt-1'>Mobile : <span className='font-semibold capitalize text-black'>{item.shippingAddress[0].phone}</span></h6>
-                      <h6 className='text-gray-600 mt-1'>Ordered date : <span className='font-semibold capitalize text-black'>{item.orderDate}</span></h6>
-                      <h6 className='text-gray-600  mt-1'>Order Id : <span className='font-semibold capitalize underline text-black'>{item._id}</span></h6>
+                    <h6 className='text-white rounded p-1 px-2 bg-blue-500 w-fit'>Order From :  <span className='font-semibold capitalize text-white'>{item.shippingAddress[0].name}</span></h6>
+                    <h6 className='text-gray-600 mt-1'>Total Amount : <span className='font-semibold capitalize text-black'>₹{item.totalAmount}</span></h6>
+                    <h6 className='text-gray-600 mt-1'>Order Status : <span className={`font-semibold capitalize ${item.orderStatus === "pending" ? "bg-white text-orange-700 rounded p-1 px-2" : ""} ${item.orderStatus === "cancelled" ? "bg-red-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "confirmed" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "delivered" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "shipped" ? "bg-green-600 text-white rounded p-1 px-2" : ""} ${item.orderStatus === "outofdelivery" ? "bg-green-600 text-white rounded p-1 px-2" : ""}`}>{item.orderStatus}</span></h6>
+                    <h6 className='text-gray-600 mt-1'>Address : <span className='font-semibold capitalize text-black'>{item.shippingAddress[0].city}</span></h6>
+                    <h6 className='text-gray-600 mt-1'>Mobile : <span className='font-semibold capitalize text-black'>{item.shippingAddress[0].phone}</span></h6>
+                    <h6 className='text-gray-600 mt-1'>Ordered date : <span className='font-semibold capitalize text-black'>{item.orderDate}</span></h6>
+                    <h6 className='text-gray-600  mt-1'>Order Id : <span className='font-semibold capitalize underline text-black'>{item._id}</span></h6>
                   </Link>
 
                 ))}
