0d0b9a9391812814d6bac76e04e37aeb890c26fb
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2026-02-08 17:00:20 +0530

    add

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,6 @@ self.addEventListener("push", (event) => {
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow(event.notification.data.url || "/admin/orders")
+    clients.openWindow(event.notification.data.url || "/")
   );
 });
